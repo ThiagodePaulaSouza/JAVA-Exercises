ab17ff8a84fff0ded2fc6649e50ab2ad4282f08f
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2021-09-08 19:40:15 -0300

    teste pesquisarPorID

--- CrudPessoas/src/DAL/PessoaDAO.java
+++ CrudPessoas/src/DAL/PessoaDAO.java
@@ -3,12 +3,15 @@ package DAL;
 import Modelo.Pessoa;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class PessoaDAO
 {
+
     private String mensagem;
     Conexao conexao = new Conexao();
+
     public void cadastrarPessoa(Pessoa pessoa)
     {
         this.mensagem = "";
@@ -16,9 +19,9 @@ public class PessoaDAO
         {
             Connection con = conexao.conectar();
             if (conexao.getMensagem().equals(""))
-            {    
+            {
                 String comSql = "insert into Pessoas (nome, rg, cpf)"
-                    + "values(?,?,?)";
+                        + "values(?,?,?)";
                 PreparedStatement stmt = con.prepareStatement(comSql);
                 stmt.setString(1, pessoa.getNome());
                 stmt.setString(2, pessoa.getRg());
@@ -27,33 +30,67 @@ public class PessoaDAO
                 conexao.desconectar();
                 this.mensagem = "Pessoa cadastrada com sucesso!!";
             }
-            else{
+            else
+            {
                 this.mensagem = conexao.getMensagem();
             }
-            
+
         }
         catch (Exception e)
         {
-            this.mensagem = "erro de gravação no BD"; // para cliente
+            this.mensagem = "erro de gravação no BD";                           // para o cliente
             //this.mensagem = e.getMessage();  para desenvolvimento
         }
     }
-    
+
     public void editarPessoa(Pessoa pessoa)
     {
-        
+
     }
-    
+
     public void excluirPessoa(Pessoa pessoa)
     {
-        
+
     }
-    
+
     public Pessoa pesquisarPessoaPorId(Pessoa pessoa)
     {
-        return null;
+        this.mensagem = "";
+        try
+        {
+            Connection con = conexao.conectar();
+            if (conexao.getMensagem().equals(""))
+            {
+                String comSql = "select * from pessoas where id = ?";
+                PreparedStatement stmt = con.prepareStatement(comSql);
+                stmt.setInt(1, pessoa.getId());
+                ResultSet resultSet = stmt.executeQuery();                      //quando se tem resposta
+                if (resultSet.next())                                           //se conseguir dar next significa que conseguiu um id
+                {
+                    pessoa.setNome(resultSet.getString("nome"));
+                    pessoa.setRg(resultSet.getString("rg"));
+                    pessoa.setCpf(resultSet.getString("cpf"));
+                }
+                else
+                {
+                    this.mensagem = "não existe este ID";
+                }
+                conexao.desconectar();
+            }
+            else
+            {
+                this.mensagem = conexao.getMensagem();
+            }
+
+        }
+        catch (Exception e)
+        {
+            this.mensagem = "erro de leitura no BD";                            // para o cliente
+            //this.mensagem = e.getMessage();  para desenvolvimento
+        }
+        return pessoa;
     }
-    
+
     public List<Pessoa> pesquisarPessoaPorNome(Pessoa pessoa)
     {
         return null;
@@ -68,6 +105,5 @@ public class PessoaDAO
     {
         this.mensagem = mensagem;
     }
-    
-    
+
 }
--- CrudPessoas/src/crudpessoas/CrudPessoas.java
+++ CrudPessoas/src/crudpessoas/CrudPessoas.java
@@ -1,13 +1,27 @@
 package crudpessoas;
 
 import Apresentacao.frmPrincipal;
+import DAL.PessoaDAO;
+import Modelo.Pessoa;
 
 public class CrudPessoas {
 
     public static void main(String[] args) 
     {
-        frmPrincipal frmP = new frmPrincipal(null, true);
-        frmP.setVisible(true);
+//        frmPrincipal frmP = new frmPrincipal(null, true);
+//        frmP.setVisible(true);
+        
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(3);
+        PessoaDAO pessoaDAO = new PessoaDAO();
+        pessoa = pessoaDAO.pesquisarPessoaPorId(pessoa);
+        if (pessoaDAO.getMensagem().equals(""))
+        {
+            System.out.println(pessoa.getId() + " " + pessoa.getNome() + "\n" +
+                    pessoa.getRg() + " " + pessoa.getCpf());
+        }
+        else
+            System.out.println(pessoaDAO.getMensagem());
     }
     
 }
